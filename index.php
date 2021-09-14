701e28c335aa66aa0978e6c079d35ecd27cce377
JlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-09-14 11:35:40 -0500

    Menu Principal
    Se creo el nabvar principal de la pagina web empresarial

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <title>Inicio | Huishilopostli Soft</title>
+  </head>
+  <body>
+  <!-- inicia mi nabvar de la pagina web -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+         <a class="navbar-brand" href="index.php">HSoft</a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNavDropdown">
+           <ul class="navbar-nav">
+               <li class="nav-item">
+                   <a class="nav-link active" aria-current="page" href="#">
+                     <svg class="bi" width="16" height="16" fill="currentColor">
+                        <use xlink:href="library/icons/bootstrap-icons.svg#house-door-fill"/>
+                     </svg> Inicio</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="#">
+                     <svg class="bi" width="16" height="16" fill="currentColor">
+                        <use xlink:href="library/icons/bootstrap-icons.svg#award"/>
+                     </svg> Quines Somos</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="#">
+                     <svg class="bi" width="16" height="16" fill="currentColor">
+                        <use xlink:href="library/icons/bootstrap-icons.svg#pen-fill"/>
+                     </svg> Politicas</a>
+               </li>
+               <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <svg class="bi" width="16" height="16" fill="currentColor">
+                         <use xlink:href="library/icons/bootstrap-icons.svg#columns-gap"/>
+                      </svg> Servicios
+                    </a>
+                   <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <li><a class="dropdown-item" href="#">Soperte Tecnico</a></li>
+                      <li><a class="dropdown-item" href="#">Seguridad Informatica</a></li>
+                      <li><a class="dropdown-item" href="#">Instalación de redes</a></li>
+                      <li><a class="dropdown-item" href="#">Creación de sitios web</a></li>
+                   </ul>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="#">Nuestro Equipo</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="#">Proyectos</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="#">Contacto</a>
+               </li>
+            </ul>
+        </div>
+      </div>
+  </nav>
+  <!-- termina mi navbar -->
+  <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
