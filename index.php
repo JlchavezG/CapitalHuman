fb68596de86d6b8286bbcfe09b8eb24ac542b0c5
JlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-09-21 09:34:59 -0500

    integracion logo
    se integra logo en la carpeta

--- index.php
+++ index.php
@@ -1,64 +1,71 @@
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
-  <head>
+<head>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <title>Inicio | Huishilopostli Soft</title>
-  </head>
-  <body>
-  <!-- inicia mi nabvar de la pagina web -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-         <a class="navbar-brand" href="index.php">HSoft</a>
-         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-         </button>
-         <div class="collapse navbar-collapse" id="navbarNavDropdown">
+</head>
+<body>
+<div class="row bg-dark text-light">
+   <div class="col-sm-1 col-md-2 col-lg-4"></div>
+   <div class="col-sm-1 col-md-2 col-lg-4"></div>
+   <div class="col-sm-10 col-md-8 col-lg-4">
+     <svg class="bi" width="16" height="16" fill="currentColor">
+         <use xlink:href="library/icons/bootstrap-icons.svg#phone-vibrate"/>
+     </svg> Telefono: 55-55-55-55 | Email: contacto@tuemail.com</div>
+</div>
+<!-- inicia mi nabvar de la pagina web -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+          <img src="img/Logo_isc.png" alt="" style="width:60px">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
            <ul class="navbar-nav">
-               <li class="nav-item">
-                   <a class="nav-link active" aria-current="page" href="#">
-                     <svg class="bi" width="16" height="16" fill="currentColor">
-                        <use xlink:href="library/icons/bootstrap-icons.svg#house-door-fill"/>
-                     </svg> Inicio</a>
-               </li>
-               <li class="nav-item">
-                   <a class="nav-link" href="#">
-                     <svg class="bi" width="16" height="16" fill="currentColor">
-                        <use xlink:href="library/icons/bootstrap-icons.svg#award"/>
-                     </svg> Quines Somos</a>
-               </li>
-               <li class="nav-item">
-                   <a class="nav-link" href="#">
-                     <svg class="bi" width="16" height="16" fill="currentColor">
-                        <use xlink:href="library/icons/bootstrap-icons.svg#pen-fill"/>
-                     </svg> Politicas</a>
-               </li>
-               <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      <svg class="bi" width="16" height="16" fill="currentColor">
-                         <use xlink:href="library/icons/bootstrap-icons.svg#columns-gap"/>
-                      </svg> Servicios
-                    </a>
-                   <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <li><a class="dropdown-item" href="#">Soperte Tecnico</a></li>
-                      <li><a class="dropdown-item" href="#">Seguridad Informatica</a></li>
-                      <li><a class="dropdown-item" href="#">Instalación de redes</a></li>
-                      <li><a class="dropdown-item" href="#">Creación de sitios web</a></li>
-                   </ul>
-               </li>
-               <li class="nav-item">
-                   <a class="nav-link" href="#">Nuestro Equipo</a>
-               </li>
-               <li class="nav-item">
-                   <a class="nav-link" href="#">Proyectos</a>
-               </li>
-               <li class="nav-item">
-                   <a class="nav-link" href="#">Contacto</a>
-               </li>
-            </ul>
-        </div>
+              <li class="nav-item">
+                 <a class="nav-link" aria-current="page" href="#">
+                   <svg class="bi" width="16" height="16" fill="currentColor">
+                       <use xlink:href="library/icons/bootstrap-icons.svg#person-bounding-box"/>
+                   </svg> Conocenos</a>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link" href="#"><svg class="bi" width="16" height="16" fill="currentColor">
+                     <use xlink:href="library/icons/bootstrap-icons.svg#building"/>
+                 </svg> Nuestros Clientes</a>
+              </li>
+              <li class="nav-item">
+                 <a class="nav-link" href="#">
+                   <svg class="bi" width="16" height="16" fill="currentColor">
+                     <use xlink:href="library/icons/bootstrap-icons.svg#cpu-fill"/>
+                   </svg> Proyectos</a>
+              </li>
+              <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <svg class="bi" width="16" height="16" fill="currentColor">
+                      <use xlink:href="library/icons/bootstrap-icons.svg#cart-plus-fill"/>
+                    </svg> Servicios
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                     <li><a class="dropdown-item" href="#">Seguridad Informática</a></li>
+                     <li><a class="dropdown-item" href="#">Instalación de Redes</a></li>
+                     <li><a class="dropdown-item" href="#">Configuración de Redes</a></li>
+                     <li><a class="dropdown-item" href="#">Aplicaciones Mobiles</a></li>
+                     <li><a class="dropdown-item" href="#">Aplicaciones Web</a></li>
+                  </ul>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="#"> Nuestro Equipo</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="#"> Contacto</a>
+             </li>
+          </ul>
       </div>
-  </nav>
+    </div>
+</nav>
   <!-- termina mi navbar -->
   <script src="js/bootstrap.min.js"></script>
   </body>
