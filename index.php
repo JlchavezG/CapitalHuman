95abcd8321c3ae281f7cf319cea7e77cd8388dcb
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-10-24 20:55:38 -0500

    cambio de imagen
    se actualiza slider y section

--- index.php
+++ index.php
@@ -105,6 +105,7 @@
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2" style="background-color:rgb(33, 97, 140);"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3" style="background-color:rgb(33, 97, 140);"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4" style="background-color:rgb(33, 97, 140);"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5" style="background-color:rgb(33, 97, 140);"></button>
         </div>
         <div class="carousel-inner py-3">
             <div class="carousel-item active">
@@ -136,6 +137,14 @@
                <div class="carousel-caption d-none d-md-block">
                   <h5 class="text-muted">¿Necesitas un sitio web?</h5>
                   <p class="text-secondary">Nosotros te ayudamos a realizarlo segun las necesidades y caracteristicas de tu negocio.</p>
+                  <a href="#" class="btn btn-sm bg-primary bg-gradient text-light">Más Información</a>
+               </div>
+            </div>
+            <div class="carousel-item">
+               <img src="img/slider5.png" class="d-block w-100" alt="slider5">
+               <div class="carousel-caption d-none d-md-block">
+                  <h5 class="text-muted">¿La mejor forma de enviar o recibir tus paquetes?</h5>
+                  <p class="text-secondary">Nuestro servicio es personalizado y 100% confidencial.</p>
                   <a href="#" class="btn btn-sm bg-primary bg-gradient text-light">Más Información</a>
                </div>
             </div>
@@ -326,8 +335,15 @@
                   <h3 class="display-6" style="color:rgb(33, 97, 140);">Examenes Psicometricos</h3>
                   <p class="text-muted text-break">Aplicación de pruebas Psicometricas y proyectivas. Es esencial para detectar la adaptabilidad de las personas que conformaran tu equipo de trabajo, evaluando la personalidad de cada individuo.</p>
                   <p class="fw-bold">Conoce los diferentes tipos de exámenes psicométricos, te apoyamos a elegir el que mejor se adapte a las necesidades de tu empresa y/o negocio.</p>
+                  <div class="row py-3">
+                      <div class="container">
+                            <div class="text-center">
+                              <a href="#" class="btn btn-sm btn-primary">Más Información</a>
+                            </div>
+                      </div>
+                  </div>
                 </div>
-                <div class="col-sm-12 col-md-6 col-lg-6 text-center">
+                <div class="col-sm-12 col-md-6 col-lg-6 text-center py-3">
                   <img src="img/Examenes.png" alt="Examenes psicometricos" id="Reclutamiento">
                 </div>
              </div>
@@ -364,6 +380,13 @@
                       </svg> Selección de talento
                     </li>
                   </ul>
+                  <div class="row py-3">
+                      <div class="container">
+                            <div class="text-center">
+                              <a href="#" class="btn btn-sm btn-primary">Más Información</a>
+                            </div>
+                      </div>
+                  </div>
                 </div>
              </div>
          </div>
@@ -396,6 +419,13 @@
                       </svg> Otros Aspectos
                     </li>
                   </ul>
+                  <div class="row py-3">
+                      <div class="container">
+                            <div class="text-center">
+                              <a href="#" class="btn btn-sm btn-primary">Más Información</a>
+                            </div>
+                      </div>
+                  </div>
                 </div>
                 <div class="col-sm-12 col-md-6 col-lg-6 text-center">
                   <img src="img/Perfiles.png" alt="Examenes psicometricos">
@@ -411,29 +441,154 @@
                 </div>
                 <div class="col">
                     <h3 class="display-6" style="color:rgb(33, 97, 140);">Organigrama Empresarial</h3>
-                  <p class="text-muted text-break">Un organigrama es necesario para planificar la comunicación interna y el flujo de trabajo, por lo que estructurar un organigrama es fundamental para agilizar los procesos.</p>
-                  <div class="row text-center py-3">
-                      <div class="col">
-                          <svg class="bi text-primary" width="35" height="35" role="img" aria-label="Tools">
-                            <use xlink:href="library/icons/bootstrap-icons.svg#diagram-3-fill"/>
-                          </svg>
-                      </div>
-                      <div class="col">
-                         <svg class="bi text-primary" width="35" height="35" role="img" aria-label="Tools">
-                           <use xlink:href="library/icons/bootstrap-icons.svg#pie-chart-fill"/>
-                         </svg>
-                      </div>
-                      <div class="col">
-                        <svg class="bi text-primary" width="35" height="35" role="img" aria-label="Tools">
-                          <use xlink:href="library/icons/bootstrap-icons.svg#ui-radios"/>
-                        </svg>
+                  <p class="text-muted text-justify py-2">Un organigrama es necesario para planificar la comunicación interna y el flujo de trabajo, por lo que estructurar un organigrama es fundamental para agilizar los procesos.</p>
+                  <div class="row text-justify py-3">
+                  <p class="fw-bold">Una representación visual de la jerarquía del personal,la estructura y flujo de la información dentro de una empresa. Es posible identificar de un modo directo las diferentes relaciones entre funciones, departamentos, equipos e individuos.</p>
+                  </div>
+                  <div class="row py-3">
+                      <div class="container">
+                            <div class="text-center">
+                              <a href="#" class="btn btn-sm btn-primary">Más Información</a>
+                            </div>
                       </div>
                   </div>
                 </div>
              </div>
          </div>
      </section>
-   <!-- termina contenido -->
+   <!-- inicia banner efecto -->
+   <div class="row py-3">
+     <div class="col-sm-2 col-md-2 col-lg-2"></div>
+     <div class="col-sm-10 col-md-10 col-lg-10 banner2 shadow">
+         <div class="container py-3">
+            <h3 class="text-center text-light display-6">Capital Human World</h3><hr class="text-light">
+            <p class="text-light">Todo nuestro personal y asesores estan capacitados para poder responder todas tus dudas sobre nustros servicios y soluciones para tu empresa y/o negocio. Selecciona cualquiera de las opciones que tenemos para contactarnos.</p>
+             <div class="row text-center py-3">
+                 <div class="col">
+                   <svg class="bi text-light" width="35" height="35" role="img" aria-label="Tools">
+                     <use xlink:href="library/icons/bootstrap-icons.svg#headset"/>
+                   </svg>
+                   <p class="text-light">Via telefonica</p>
+                 </div>
+                 <div class="col">
+                   <svg class="bi text-light" width="35" height="35" role="img" aria-label="Tools">
+                     <use xlink:href="library/icons/bootstrap-icons.svg#whatsapp"/>
+                   </svg>
+                   <p class="text-light">Via whatsapp</p>
+                 </div>
+                 <div class="col">
+                   <svg class="bi text-light" width="35" height="35" role="img" aria-label="Tools">
+                     <use xlink:href="library/icons/bootstrap-icons.svg#mailbox2"/>
+                   </svg>
+                   <p class="text-light">Via Email</p>
+                 </div>
+             </div>
+             <div class="row">
+                 <div class="container text-center py-3">
+                    <button type="button" name="button" class="btn btn-sm btn-info text-light">Descarga nuestra carta de presentación</button>
+                 </div>
+             </div>
+         </div>
+     </div>
+   </div>
+   <!-- termina banner -->
+   <section id="Manuales" class="py-4">
+       <div class="container">
+           <div class="row">
+              <div class="col-sm-12 col-md-6 col-lg-6">
+                <h3 class="display-6" style="color:rgb(33, 97, 140);">Manuales de procedimientos</h3>
+                <p class="text-muted text-justify py-2">Te ayudamos a crear un documento del sistema de Control Interno de tu empresa, el cual se crea para obtener una información detallada, ordenada, sistemática e integral que contiene todas las instrucciones, responsabilidades e información sobre políticas, funciones, sistemas y procedimientos de las distintas operaciones.</p>
+              </div>
+              <div class="col-sm-12 col-md-6 col-lg-6 text-center">
+                <img src="img/Manual.png" alt="Examenes psicometricos">
+              </div>
+           </div>
+       </div>
+   </section>
+   <section id="Organigrama" class="py-1">
+       <div class="container">
+           <div class="row ">
+              <div class="col-sm-12 col-md-6 col-lg-6 text-center">
+                  <img src="img/Contratos.png" alt="Seleccion de personal">
+              </div>
+              <div class="col">
+                  <h3 class="display-6" style="color:rgb(33, 97, 140);">Elaboración de Contratos</h3>
+                <p class="text-muted text-break">Un organigrama es necesario para planificar la comunicación interna y el flujo de trabajo, por lo que estructurar un organigrama es fundamental para agilizar los procesos.</p>
+                <div class="row text-center py-3">
+                    <div class="col">
+                        <svg class="bi text-primary" width="35" height="35" role="img" aria-label="Tools">
+                          <use xlink:href="library/icons/bootstrap-icons.svg#diagram-3-fill"/>
+                        </svg>
+                    </div>
+                    <div class="col">
+                       <svg class="bi text-primary" width="35" height="35" role="img" aria-label="Tools">
+                         <use xlink:href="library/icons/bootstrap-icons.svg#pie-chart-fill"/>
+                       </svg>
+                    </div>
+                    <div class="col">
+                      <svg class="bi text-primary" width="35" height="35" role="img" aria-label="Tools">
+                        <use xlink:href="library/icons/bootstrap-icons.svg#ui-radios"/>
+                      </svg>
+                    </div>
+                </div>
+              </div>
+           </div>
+       </div>
+   </section>
+   <section id="Manuales" class="py-4">
+       <div class="container">
+           <div class="row">
+              <div class="col-sm-12 col-md-6 col-lg-6">
+                <h3 class="display-6" style="color:rgb(33, 97, 140);">Manuales de procedimientos</h3>
+                <p class="text-muted text-break">Te ayudamos a crear un documento del sistema de Control Interno de tu empresa, el cual se crea para obtener una información detallada, ordenada, sistemática e integral que contiene todas las instrucciones, responsabilidades e información sobre políticas, funciones, sistemas y procedimientos de las distintas operaciones.</p>
+              </div>
+              <div class="col-sm-12 col-md-6 col-lg-6 text-center">
+                <img src="img/Manual.png" alt="Examenes psicometricos">
+              </div>
+           </div>
+       </div>
+   </section>
+   <!-- inicia session -->
+   <section id="Nostros">
+       <div class="row py-5">
+           <div class="col-sm-8 col-md-8 col-lg-8 bg-info py-3 shadow">
+               <h2 class="text-center text-light display-6"> Nosotros</h2>
+               <hr class="text-light">
+               <div class="container">
+                  <p class="text-light text-justify py-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                  fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.</p>
+                  <div class="row text-center">
+                      <div class="col">
+                        <a data-bs-toggle="offcanvas" href="#offcanvasCalidad" class="text-decoration-none" role="button" aria-controls="offcanvasExample"><svg class="bi text-light" width="42" height="42" fill="currentColor">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#bookmark-fill"/>
+                        </svg>
+                        <h5 class="text-light">Calidad</h5></a>
+                      </div>
+                      <div class="col">
+                        <a data-bs-toggle="offcanvas" href="#offcanvasServicio" class="text-decoration-none"><svg class="bi text-light" width="42" height="42" fill="currentColor">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#cup-fill"/>
+                        </svg>
+                        <h5 class="text-light">Servicio</h5></a>
+                      </div>
+                      <div class="col">
+                        <svg class="bi text-light" width="42" height="42" fill="currentColor">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#shield-lock-fill"/>
+                        </svg>
+                        <h5 class="text-light">Seguridad</h5>
+                      </div>
+                  </div>
+               </div>
+           </div>
+           <div class="col-sm-4 col-md-4 col-lg-4 text-center py-5">
+
+           </div>
+       </div>
+   </section>
+   <!-- termina seccion -->
    </div>
    <!-- inicia footer -->
    <footer class="text-white text-center text-lg-start" style="background-color:rgb(33, 97, 140);">
@@ -470,10 +625,8 @@
             </li>
           </ul>
         </div>
-
         <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
           <h5 class="text-uppercase mb-4">Horarios de atención</h5>
-
           <table class="table text-center text-white">
             <tbody class="font-weight-normal">
               <tr>
@@ -487,17 +640,11 @@
             </tbody>
           </table>
         </div>
-        <!--Grid column-->
       </div>
-      <!--Grid row-->
     </div>
-    <!-- Grid container -->
-
-    <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
       © 2021 Copyright:CapitalHunanWorld | IscjlchavezG
     </div>
-    <!-- Copyright -->
   </footer>
    <!-- termina footer -->
      <script src="js/bootstrap.min.js"></script>
