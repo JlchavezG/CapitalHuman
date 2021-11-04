984f9341220e5d948203fe7ef9319f40375578aa
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-11-04 10:16:03 -0600

    navar y perfil
    se modificaron estructura del perfil y del navbar general esperando más cambios del cliente

--- Include/navbar.php
+++ Include/navbar.php
@@ -15,6 +15,22 @@
                 </div>
                 <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbarJl">
                    <ul class="navbar-nav ms-auto flex-nowrap">
+                     <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle selectMenu" href="#Soluciones" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Bienvenido: <?php echo $user['Nombre']; ?>&nbsp;&nbsp;<img src="img/user/<?php echo $user['Imgen']; ?>" alt="Perfil" style="width:30px; border-radius:50%;">
+                        </a>
+                      <ul class="dropdown-menu menus" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="Perfil.php"><svg class="bi" width="17" height="17" role="img" aria-label="Tools">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#person-badge"/>
+                          </svg> Perfil</a></li>
+                        <li><a class="dropdown-item" href="#Reclutamiento"><svg class="bi" width="17" height="17" role="img" aria-label="Tools">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#gear-fill"/>
+                          </svg> Configuración</a></li>
+                        <li><a class="dropdown-item" href="include/cerrar.php"><svg class="bi" width="17" height="17" role="img" aria-label="Tools">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#door-open-fill"/>
+                          </svg> Salir</a></li>
+                      </ul>
+                     </li>
                      <li class="nav-item">
                         <a href="#" class="nav-link"><svg class="bi" width="17" height="17" role="img" aria-label="Tools">
                             <use xlink:href="library/icons/bootstrap-icons.svg#bell-fill"/>
@@ -23,28 +39,6 @@
                           </span>
                           </a>
                      </li>
-                     <li class="nav-item">
-                        <a href="#" class="nav-link"><svg class="bi" width="17" height="17" role="img" aria-label="Tools">
-                            <use xlink:href="library/icons/bootstrap-icons.svg#mailbox2"/>
-                            </svg></a>
-                     </li>
-                      <!-- nuetro navdrop-->
-                      <li class="nav-item dropdown">
-                         <a class="nav-link dropdown-toggle selectMenu" href="#Soluciones" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                           Bienvenido: <?php echo $user['Nombre']; ?>&nbsp;&nbsp;<img src="img/user/<?php echo $user['Imgen']; ?>" alt="Perfil" style="width:30px; border-radius:50%;">
-                         </a>
-                       <ul class="dropdown-menu menus" aria-labelledby="navbarDropdown">
-                         <li><a class="dropdown-item" href="Perfil.php"><svg class="bi" width="17" height="17" role="img" aria-label="Tools">
-                             <use xlink:href="library/icons/bootstrap-icons.svg#person-badge"/>
-                           </svg> Perfil</a></li>
-                         <li><a class="dropdown-item" href="#Reclutamiento"><svg class="bi" width="17" height="17" role="img" aria-label="Tools">
-                             <use xlink:href="library/icons/bootstrap-icons.svg#gear-fill"/>
-                           </svg> Configuración</a></li>
-                         <li><a class="dropdown-item" href="include/cerrar.php"><svg class="bi" width="17" height="17" role="img" aria-label="Tools">
-                             <use xlink:href="library/icons/bootstrap-icons.svg#door-open-fill"/>
-                           </svg> Salir</a></li>
-                       </ul>
-                      </li>
                       <li class="nav-item">
                          <a href="#" class="nav-link"><svg class="bi" width="17" height="17" role="img" aria-label="Tools">
                              <use xlink:href="library/icons/bootstrap-icons.svg#instagram"/>
--- Perfil.php
+++ Perfil.php
@@ -14,7 +14,58 @@
    <?php include 'include/navbar.php';?>
   <!-- termina nabvar -->
   <!-- crear el contenedor del perfil -->
-    
+    <div class="container py-5">
+       <div class="row d-flex justify-content-center py-5">
+          <div class="col py-4">
+              <div class="card shadow">
+                   <div class="container ">
+                       <div class="row py-3 justify-content-center">
+                           <img src="img/user/<?php echo $user['Imgen']; ?>" alt="Imagen de perfil" style="width:250px;" class="rounded-circle">
+                           <div class="py-1"><hr></div>
+                           <h3 class="text-center"><?php echo $user['Nombre'];?> <?php echo $user['ApellidoP'];?> <?php echo $user['ApellidoM'];?></h3>
+                           <div class="row text-center">
+                               <span class="text-primary">Email:<?php echo $user['Email']; ?></span>
+                           </div>
+                       </div>
+                   </div>
+              </div>
+          </div>
+          <div class="col py-4">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Tipo de Usuario:</li>
+              <li class="list-group-item">Telefono:</li>
+              <li class="list-group-item">Genero:</li>
+              <li class="list-group-item">Empresa:</li>
+              <li class="list-group-item">Proyecto:</li>
+            </ul>
+            <div class="container">
+                 <div class="row py-2 text-center">
+                      <div class="col">
+                       <span>
+                            <svg class="bi" width="20" height="20" role="img" aria-label="Tools">
+                            <use xlink:href="library/icons/bootstrap-icons.svg#printer-fill"/>
+                            </svg>&nbsp; Imprimir
+                        </span>
+                      </div>
+                      <div class="col">
+                        <span>
+                           <svg class="bi" width="20" height="20" role="img" aria-label="Tools">
+                           <use xlink:href="library/icons/bootstrap-icons.svg#cloud-download-fill"/>
+                           </svg>&nbsp; Descargar
+                         </span>
+                      </div>
+                      <div class="col">
+                        <span>
+                           <svg class="bi" width="20" height="20" role="img" aria-label="Tools">
+                           <use xlink:href="library/icons/bootstrap-icons.svg#pencil-fill"/>
+                           </svg>&nbsp; Modificar
+                        </span>
+                      </div>
+                 </div>
+            </div>
+          </div>
+       </div>
+    </div>
   <!-- termina el contenedor del perfil -->
   <script src="js/bootstrap.min.js"></script>
   </body>
