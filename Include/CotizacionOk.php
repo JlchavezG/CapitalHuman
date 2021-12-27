431004fa77b409553c05e827124312d50a78b146
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-12-26 22:30:44 -0600

    cambio en cotizacion
    se cambian en cotizacion

--- Include/CotizacionOk.php
+++ Include/CotizacionOk.php
@@ -6,6 +6,7 @@ $TelefonoC = $conecta->real_escape_string($_POST['Ctelefono']);
 $EmailC = $conecta->real_escape_string($_POST['Cemail']);
 $FechaC = date('Y-m-d');
 $fechaF = date('d-m-Y');
+$Estatus = '0';
 $caracteres ="123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-.#!";
 for ($i=0; $i < 10 ; $i++) {
   $token = substr(str_shuffle($caracteres),0,10);
@@ -20,6 +21,52 @@ $Servicio = $conecta->real_escape_string($_POST['servicio']);
 $Tservicio = "SELECT * FROM Servicios WHERE Id_servicio = '$Servicio'";
 $Tservicios = $conecta->query($Tservicio);
 $SelectServ = $Tservicios->fetch_assoc();
+// insertar en bd la cotizacion
+$insertCotiza = "INSERT INTO SCotizacion(Solicitante, Fecha, Id_Servicio, Telefono, Email, MComunicación, Estatus, Token)VALUES('$Atencion','$FechaC','$Tservicios','$TelefonoC','$EmailC','$Tcontacto','$Estatus','$token')";
+$inserta = $conecta->query($insertCotiza);
+if($inserta > 0){
+  $cotiza.= "<div class='row content-justify-center'>
+                 <div class='col-sm-12 col-md-12 col-lg-12 py-2'>
+                     <div class='card shadow'>
+                        <span class='text-muted text-center py-3'> Datos de la cotización</span>
+                        <div class='card-body'>
+                           <h6 class='text-center'> Gracias por solicitar tu Cotización <span class='text-primary'><?php echo $Atencion; ?></span></h6>
+                               <ul class='list-group list-group-flush'>
+                                 <li class='list-group-item'>Fecha de envio : <span class='text-primary'><?php echo $fechaF; ?></span></li>
+                                 <li class='list-group-item'>Token de Cotización : <span class='text-primary'><?php echo $token; ?></span></li>
+                                 <li class='list-group-item'>Atención a : <span class='text-primary'><?php echo $Atencion; ?></span></li>
+                                 <li class='list-group-item'>Metodo de contacto :<span class='text-primary'> <?php echo $Tcontacto; ?></span></li>
+                                 <li class='list-group-item'>Servicio a cotizar : <span class='text-primary'><?php echo $SelectServ['TServicio']; ?></span></li>
+                               </ul>
+                        </div>
+                     </div>
+               </div>
+               <div class="col-sm-12 col-md-12 col-lg-12 py-2">
+          <div class="card shadow">
+             <div class="row py-3 text-center">
+                 <span class="text-muted text-center"> Menu</span>
+                 <div class="col">
+                     <div class="container">
+                       <svg class="bi text-primary" width="17" height="17" role="img" aria-label="Tools">
+                          <use xlink:href="../library/icons/bootstrap-icons.svg#printer-fill"/>
+                       </svg><a href="javascript:window.print()" class="text-muted text-decoration-none"> Imprimir</a> |
+                       <svg class="bi text-primary" width="17" height="17" role="img" aria-label="Tools">
+                          <use xlink:href="../library/icons/bootstrap-icons.svg#whatsapp"/>
+                       </svg><a href="whatsapp://send?text=texto%20con%20URL" class="text-muted text-decoration-none"> Compartir</a> |
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="row py-4 ">
+          <div class="text-center">
+            <img src="../img/logo_human.png" alt="logo_capital_human" style="width:250px;">
+          </div>
+         </div>
+      </div>
+  </div>";
+}
+
+
 }
 else{
   header('location:../index.php');
@@ -30,29 +77,12 @@ else{
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../css/main.css">
     <link rel="stylesheet" type="text/css" href="../css/pace.css">
     <title>Cotización</title>
   </head>
   <body>
-    <div class="container mt-3">
-          <div class="row content-justify-center">
-              <div class="col-sm-6 col-md-6 col-lg-6 py-2">
-                  <div class="card shadow">
-                    <span class="text-muted text-center py-3"> Datos de la cotización</span>
-                    <div class="card-body">
-                       <h6 class="text-center"> Gracias por solicitar tu Cotización <span class="text-primary"><?php echo $Atencion; ?></span></h6>
-                       <ul class="list-group list-group-flush">
-                          <li class="list-group-item">Fecha de envio : <span class="text-primary"><?php echo $fechaF; ?></span></li>
-                          <li class="list-group-item">Token de Cotización : <span class="text-primary"><?php echo $token; ?></span></li>
-                          <li class="list-group-item">Atención a : <span class="text-primary"><?php echo $Atencion; ?></span></li>
-                          <li class="list-group-item">Metodo de contacto :<span class="text-primary"> <?php echo $Tcontacto; ?></span></li>
-                          <li class="list-group-item">Servicio a cotizar : <span class="text-primary"><?php echo $SelectServ['TServicio']; ?></span></li>
-                       </ul>
-                    </div>
-                  </div>
-              </div>
-          </div>
+    <div class="container mt-3" id="print">
+
     </div>
   <script src="../js/bootstrap.min.js"></script>
   </body>
